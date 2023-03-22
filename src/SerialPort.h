0c6a496a5ed5d00d0f4ad9bc8691f7f2ba55f68a
Sergey Zaplatnikov <s.zaplatnikov@constantrobotics.com>
2023-03-22 11:09:39 +0200

    Updated

--- src/SerialPort.h
+++ src/SerialPort.h
@@ -51,8 +51,7 @@ public:
 
     /**
      * @brief Method to open serial port.
-     *
-     * @param comport_file Serial port name string. Format depends from OS.
+     * @param file Serial port name string. Format depends from OS.
      * @param baudrate Boudrate.
      * @param timeout Wait data timeout.
      * @param mode Mode. Always 3 simbols:
@@ -60,28 +59,23 @@ public:
      * 2 - parity (N, E, O),
      * 3 - number of stop bits (1 or 2).
      * Example: "8N1".
-     *
      * @return TRUE in case success or FALSE in case any errors.
      */
-    bool open(const char *comport_file, unsigned int baudrate,
+    bool open(const char *file, unsigned int baudrate,
               unsigned int timeout = 100, const char *mode = "8N1");
 
     /**
      * @brief Read data from serial port.
-     *
      * @param buf pointer to data buffer to copy.
      * @param size size of data buffer.
-     *
      * @return Number of readed bytes or returns -1.
      */
     int readData(uint8_t *buf, uint32_t size);
 
     /**
      * @brief Send data to serial port.
-     *
      * @param buf pointer to data to send.
      * @param size size of data to send.
-     *
      * @return Number of bytes sended or return -1 in case any errors.
      */
     int sendData(uint8_t *buf, uint32_t size);
@@ -98,10 +92,8 @@ public:
     void close();
 
     /**
-     * @brief Method to set RTS/CTS hardware flow control
-     *
-     * @param enable Enable RTS/CTS hardware flow control
-     *
+     * @brief Method to set RTS/CTS hardware flow control.
+     * @param enable Enable RTS/CTS hardware flow control.
      * @return TRUE in case success or FALSE in case any errors.
      */
     bool setFlowControl(bool enable);
@@ -109,14 +101,14 @@ public:
 private:
 
     /// Port initialization flag.
-    bool m_initFlag;
+    bool m_initFlag{false};
     /// Read data timeout.
-    int m_timeoutMs;
+    int m_timeoutMs{0};
     /// Port handle.
 #if defined(linux) || defined(__linux) || defined(__linux__)|| defined(__FreeBSD__)
-    int Cport;
+    int m_port{0};
 #elif defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
-    HANDLE Cport;
+    HANDLE m_port{0};
 #endif
 
 };
